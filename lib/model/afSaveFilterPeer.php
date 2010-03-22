1e4329e6ba3ffc0e31f7adc160b744c67d41920c
prakash@immune.dk <prakash@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-03-22 10:44:19 +0000

    #1703 added methods

--- lib/model/afSaveFilterPeer.php
+++ lib/model/afSaveFilterPeer.php
@@ -2,4 +2,20 @@
 
 class afSaveFilterPeer extends BaseafSaveFilterPeer
 {
+	public static function getAllFilters()
+	{
+		$c = new Criteria();
+		$user = sfContext::getInstance()->getUser()->getGuardUser()->getId();
+		$c->add(self::USER,$user);	
+		$c->addAscendingOrderByColumn(afSaveFilterPeer::TITLE);	
+		return $c;
+	}
+	public static function hasFilters(){
+		$c = new Criteria();
+		$user = sfContext::getInstance()->getUser()->getGuardUser()->getId();
+		$c->add(self::USER,$user);
+		$filter = afSaveFilterPeer::doSelectOne($c);
+		if($filter) return true;
+		return false;
+	}
 }
