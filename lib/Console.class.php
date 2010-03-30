7f4227eae941a189e381bdf38f354342c4232526
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-03-30 11:24:47 +0000

    i#1744 moved Console under appflower.

---
+++ lib/Console.class.php
@@ -0,0 +1,39 @@
+<?php
+
+class Console {
+    private static $startedAt = null;
+    private static $last;
+
+    /**
+     * Logs the given arguments to stderr.
+     * Usage: debug($var1, $anArray, ...)
+     */
+    public static function debug() {
+        $message = 'DEBUG:';
+        foreach (func_get_args() as $arg) {
+            $message .= ' '.var_export($arg, true);
+        }
+        error_log($message);
+    }
+
+    /**
+     * Logs the number of milliseconds till this named point.
+     */
+    public static function profile($point) {
+        if(ini_get('display_errors') !== 'on') {
+            return;
+        }
+
+        $now = microtime(true);
+        if(self::$startedAt === null) {
+            self::$startedAt = $now;
+            self::$last = $now;
+        }
+
+        $totalMillis = ($now - self::$startedAt) * 1000;
+        $passedMillis = ($now - self::$last) * 1000;
+        error_log(sprintf('%dms (%dms) till %s',
+            $totalMillis, $passedMillis, $point));
+        self::$last = $now;
+    }
+}
