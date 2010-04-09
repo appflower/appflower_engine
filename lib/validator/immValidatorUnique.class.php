13a6295922041788952bce6c3dd6005375159a8e
tamas@immune.dk <tamas@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-04-09 11:58:56 +0000

    #1789 validator now supports wizards

--- lib/validator/immValidatorUnique.class.php
+++ lib/validator/immValidatorUnique.class.php
@@ -26,6 +26,7 @@ class immValidatorUnique extends sfValidatorBase
 	{	
 		$id = sfContext::getInstance()->getRequest()->getParameter("edit[0][id]",false);
 		if(!$id) $id = sfContext::getInstance()->getRequest()->getParameter("edit[1][id]",false);
+		if(!$id) $id = sfContext::getInstance()->getRequest()->getParameter("edit[2][id]",false);
 		
 		$c = new Criteria();
 		$c->add(constant($this->getOption('class')."Peer::".strtoupper($this->getOption('column'))),$value);
