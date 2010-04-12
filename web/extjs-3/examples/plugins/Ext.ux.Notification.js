b5e3ac3753ea67bb91c18be32329903911d49d0a
prakash@immune.dk <prakash@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-04-12 11:14:29 +0000

    #1774 notification js

---
+++ web/extjs-3/examples/plugins/Ext.ux.Notification.js
@@ -0,0 +1,173 @@
+Ext.ux.NotificationMgr = {
+    positions: [],
+    heights: []
+};
+Ext.ux.Notification = function(config){
+	this.config = Ext.apply({},config); 
+}
+Ext.ux.Notification = Ext.extend(Ext.Window, {
+	notificationType:'ERROR',
+	initComponent : function(){
+		Ext.apply(this, {
+			iconCls: this.iconCls || 'icon-notification-info',
+			autoHeight: true,
+			closable: true,
+			plain: false,
+			draggable: false,
+			bodyStyle: 'text-align:left;padding:10px;',
+			resizable: false
+		});
+		if(this.autoDestroy){
+			this.task = new Ext.util.DelayedTask(this.close, this);
+		}else{
+			this.closable = true;
+		}
+		Ext.ux.Notification.superclass.initComponent.call(this);
+    },
+	setMessage : function(msg){
+		this.body.update(msg);
+	},
+	setTitle : function(title, iconCls){
+        Ext.ux.Notification.superclass.setTitle.call(this, title, iconCls||this.iconCls);
+    },
+    onRender : function(ct, position) {
+		Ext.ux.Notification.superclass.onRender.call(this, ct, position);
+	},
+	onDestroy : function(){
+		Ext.ux.NotificationMgr.positions.remove(this.pos);
+		Ext.ux.Notification.superclass.onDestroy.call(this);
+	},
+	afterShow : function(){
+		Ext.ux.Notification.superclass.afterShow.call(this);
+		this.on('move', function(){
+			Ext.ux.NotificationMgr.positions.remove(this.pos);
+			if(this.autoDestroy){
+				this.task.cancel();
+			}
+		}, this);
+		if(this.autoDestroy){
+			this.task.delay(this.hideDelay || 5000);
+		}
+	},
+	animShow : function(){
+		this.pos = 0;
+		var h = 0;
+		while(Ext.ux.NotificationMgr.positions.indexOf(this.pos)>-1){
+			h+=Ext.ux.NotificationMgr.heights[this.pos];
+			this.pos++;			
+		}
+		Ext.ux.NotificationMgr.positions.push(this.pos);		
+		Ext.ux.NotificationMgr.heights[this.pos] = this.getSize().height+10;
+		
+		this.el.alignTo(this.animateTarget || document, "br-tr", [ -1, -1-(h) ]);
+		this.el.slideIn('b', {
+			duration: .7
+			, callback: this.afterShow
+			, scope: this
+		});
+	},
+	animHide : function(){
+		Ext.ux.NotificationMgr.positions.remove(this.pos);
+		this.el.ghost("b", {
+			duration: 1
+			, remove: true
+		});
+	},
+	start: function(url){
+		new Ext.ux.Notification.start(url);
+	}
+});
+Ext.ux.Notification.start = function(url){	
+	Ext.onReady(function(){	
+		var notificationEl = Ext.DomHelper.append(Ext.getBody(),{tag:'div',style:'width:300px;position:absolute;bottom:0;right:0'});			
+		this.showNotification = function(config) {
+		    var win = new Ext.ux.Notification(Ext.apply({
+		    	animateTarget: notificationEl
+				, autoDestroy: false					
+				,notificationType:'ERROR'
+		    }, config));
+		    if(win.notificationType == "ERROR"){
+		    	win.iconCls = 'icon-notification-error';
+		    	win.bodyStyle += 'background-color:#fddcdc;';
+		    }
+		    if(win.notificationType == "WARNING"){
+		    	win.iconCls = 'icon-notification-warning';
+		    	win.bodyStyle += 'background-color:#fefcc7;';
+		    }
+		    if(win.notificationType == "INFO"){
+		    	win.iconCls = 'icon-notification-info';
+		    	win.bodyStyle += 'background-color:#fff;';
+		    }
+		    win.bodyStyle += 'padding-bottom:20px;';
+		    win.show();
+	
+		    return win;
+		};
+		this.hideNotification = function(win, delay) {
+		    if (win) {
+		      (function() { 
+		        win.animHide();	        
+		      }).defer(delay || 3000);
+		    }
+		};
+		this.growl = function(TITLE,MESSAGE,TYPE,DURATION){			
+			var n = this.showNotification({
+				width:300,	
+				notificationType:TYPE,
+				title:TITLE,
+				html:MESSAGE
+			});
+			this.hideNotification(n, DURATION*1000);			
+		}		
+		this.sendRequest = {
+			url: url,
+			success: function(response){
+				var obj = Ext.decode(response.responseText);
+				if(obj){
+					var count = 0;
+					var max_duration = 0;
+					var defer = 500;
+					Ext.each(obj.data,function(d){
+						count++;
+						if(d.duration > max_duration) max_duration = d.duration;
+						var arr = [];
+						arr.push(d.title);
+						arr.push(d.message);
+						arr.push(d.type);
+						arr.push(d.duration);
+						this.growl.defer(defer*count,this,arr);						
+					},this)					
+					new Ext.util.DelayedTask(function(){
+						this.ajax.request(Ext.apply(this.sendRequest,{
+							params:{
+								limit:(3-Ext.ux.NotificationMgr.positions.length)
+							}
+						}))
+					}).delay(count*defer+1000);
+				}
+			}
+		}
+		this.ajax = Ext.Ajax;
+		this.ajax.timeout = 0;  
+		this.ajax.request(this.sendRequest);
+		this.ajax.on("requestcomplete",function(con,response,options){
+			
+		},this)
+		function newExcitingAlerts() {
+		    var oldTitle = document.title;
+		    var msg = "New Notification !! " +oldTitle;
+		    var timeoutId = setInterval(function() {
+		    	//window.focus();
+		        document.title = document.title == msg ? ' ' : msg;
+		        
+		    }, 1000);
+		    window.onmousemove = function() {
+		        clearInterval(timeoutId);
+		        document.title = oldTitle;
+		        window.onmousemove = null;
+		    };
+		}
+		//newExcitingAlerts()
+	})
+}
+
