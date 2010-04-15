dccab1dc302d93d64e091f987e400b77a6baee73
prakash@immune.dk <prakash@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-04-15 05:11:45 +0000

    153 added an overridden method for textfield to trim the value

--- web/extjs-3/examples/overrides/Override.Ext.form.Field.js
+++ web/extjs-3/examples/overrides/Override.Ext.form.Field.js
@@ -85,7 +85,34 @@ Ext.override(Ext.form.Field,{
 	    this.el.on("focus", this.onFocus, this);
 	    this.el.on("blur", this.onBlur, this);
 	}
-})
+});
+
+/**
+ * The textfield shows 10.00 as 10, but 10.01 as 10.01. We need 10.00 to be 10.00 not 10
+ * Fix for http://tickets.appflower.com/ticket/editView?id=153
+ * @author: Prakash Paudel
+ */
+Ext.override(Ext.form.TextField,{
+	initValue : function(){
+		if(this.value){			
+			this.value = this.value.toString().trim();
+		}
+	    if(this.value !== undefined){
+	        this.setValue(this.value);
+	    }else if(!Ext.isEmpty(this.el.dom.value) && this.el.dom.value != this.emptyText){
+	        this.setValue(this.el.dom.value);
+	    }
+	    
+	    /**
+	     * The original value of the field as configured in the {@link #value} configuration, or
+	     * as loaded by the last form load operation if the form's {@link Ext.form.BasicForm#trackResetOnLoad trackResetOnLoad}
+	     * setting is true.
+	     * @type mixed
+	     * @property originalValue
+	     */
+	    this.originalValue = this.getValue();
+	}
+});
 
 
 
