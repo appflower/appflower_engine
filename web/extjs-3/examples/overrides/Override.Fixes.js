a7640b6e0f4824222d8557bdd9cad3167ce26d5a
prakash@immune.dk <prakash@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-04-15 06:41:05 +0000

    #176 restored missing file

---
+++ web/extjs-3/examples/overrides/Override.Fixes.js
@@ -0,0 +1,12 @@
+Ext.lib.Event.resolveTextNode = Ext.isGecko ? function(node){
+	if(!node){
+		return;
+	}
+	var s = HTMLElement.prototype.toString.call(node);
+	if(s == '[xpconnect wrapped native prototype]' || s == '[object XULElement]'){
+		return;
+	}
+	return node.nodeType == 3 ? node.parentNode : node;
+} : function(node){
+	return node && node.nodeType == 3 ? node.parentNode : node;
+};
