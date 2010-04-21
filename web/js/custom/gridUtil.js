36b4d21aeeb4b1642628591a692e82f057ddb5f1
prakash@immune.dk <prakash@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-04-21 10:21:58 +0000

    Grid util js

---
+++ web/js/custom/gridUtil.js
@@ -0,0 +1,43 @@
+var gridUtil = function(grid,config){
+	config = Ext.decode(config);	
+	if(!grid){alert("Component not found"); return;}
+	
+	if(config.confirmMsg){
+		Ext.Msg.show({
+			title:'Confirmation required.',
+			msg: config.confirmMsg,
+			buttons: Ext.Msg.YESNOCANCEL,
+			fn: function(btn,text){
+				if (btn == 'yes'){
+			        proceed();
+			    }
+			},
+			icon: Ext.MessageBox.QUESTION
+		})
+	}else{
+		proceed();
+	}
+	var proceed = function(){
+		if(config.is_ajax){
+			if(config.url == "#") return;
+			Ext.Ajax.request({
+				url: config.url,
+				params:config.params,
+				success: function(response){
+					if(config.onsuccess == "RELOAD"){
+						grid.getStore().reload();
+						if(grid.getSelectionModel){
+							var sm = grid.getSelectionModel();
+							sm.clearSelections();
+						}
+					}
+					if(config.onsuccess == "REDIRECT"){
+						window.location.href = config.redirect;
+					}
+				}
+			})
+		}else{		
+			window.location.href = config.url
+		}
+	}
+}
