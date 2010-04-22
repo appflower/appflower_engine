43cb1065afdcbf80b702bdd44281480339d2ff75
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-04-22 14:25:41 +0000

    #1742 allowed dot in server names.

--- lib/validator/immValidatorName.class.php
+++ lib/validator/immValidatorName.class.php
@@ -3,7 +3,7 @@ class immValidatorName extends sfValidatorString
 {	
 	public function getPattern()
 	{
-		return '/^([\s\x{00c0}-\x{01ff}a-zA-Z\'\-0-9_])+$/u';
+		return '/^([\s\x{00c0}-\x{01ff}a-zA-Z\'\-0-9_.])+$/u';
 	}
 
 	protected function configure($options = array(), $messages = array())
