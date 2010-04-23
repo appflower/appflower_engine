e99f30614bf2eb03f61fad3f673d755aedf05894
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-04-23 09:39:17 +0000

    i#1818 used a loop instead of recursion.

--- web/extjs-3/examples/treegrid/Ext.ux.SynchronousTreeExpand.js
+++ web/extjs-3/examples/treegrid/Ext.ux.SynchronousTreeExpand.js
@@ -6,13 +6,13 @@ Ext.ux.SynchronousTreeExpand = function(config){
 	var expandedNodes = [];
 	var mask;
 	var moveFurther = function(){
-		if(counter >= ds.getCount()) return null;		
-		var record = store.getAt(counter);
-		counter++;
-		if(!store.isLeafNode(record) && record.get("name").match(/<font color=red>&darr;<\/font>/)){				
-			return record;
-		}else{			
-			return moveFurther();
+		while (true) {
+			if(counter >= ds.getCount()) return null;		
+			var record = store.getAt(counter);
+			counter++;
+			if(!store.isLeafNode(record) && record.get("name").match(/<font color=red>&darr;<\/font>/)){				
+				return record;
+			}
 		}
 	}
 	config.grid.store.on('load',function(){		
