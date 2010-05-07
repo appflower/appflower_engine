3292d38e85c35de9d2301244a6274b6f1519bee8
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-05-07 14:52:03 +0000

    #215 merged -c426, -c427, -c428 to AF trunk.

---
+++ modules/afApi/actions/actions.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class afApiActions extends sfActions
+{
+    public function executeListjson() {
+        //TODO: parse the datasource from the XML config
+        return $this->renderText('Not implemented yet');
+    }
+}
