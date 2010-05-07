9699e8ec5f4a726af8465f349f2af7aab2c09886
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-05-07 11:40:53 +0000

    #215 added some appFlower unit tests.

---
+++ test/bootstrap/dbunit.php
@@ -0,0 +1,8 @@
+<?php
+require_once(dirname(__FILE__).'/../../../../config/ProjectConfiguration.class.php');
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'test', true);
+new sfDatabaseManager($configuration);
+sfContext::createInstance($configuration);
+error_reporting(E_ALL);
+ 
+include($configuration->getSymfonyLibDir().'/vendor/lime/lime.php');
