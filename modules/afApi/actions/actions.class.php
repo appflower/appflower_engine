c4e42ea7b79256c28f69e9fc407f5f2e1411deba
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-05-12 08:48:03 +0000

    #215 removed an old code.

--- modules/afApi/actions/actions.class.php
+++ modules/afApi/actions/actions.class.php
@@ -69,54 +69,11 @@ class afApiActions extends sfActions
         }
     }
 
-    private function oldCode() {
-        // TODO: remove the old code
-        $data = array(
-            'uid' => $unique_id,
-            'title' => $view->get('title'),
-            'tree' => $view->getBool('fields@tree'),
-            'type' => $view->get('datasource@type'),
-            'columns' => explode(',', $view->get('display/visible')),
-            //TODO: implement ...
-
-            "group_field" => $group_field,
-            "datafile" => (isset($this->attribute_holder["datafile"])) ? $this->attribute_holder["datafile"] : null,
-            "realtime" => (isset($this->attribute_holder["realtime"])) ? $this->attribute_holder["realtime"] : false, 
-            "wizard" => $this->step,
-
-            "levels" => (isset($parse["levels"])) ? $parse["levels"] : false,
-            "datasource" => (isset($parse["datasource"])) ? $parse["datasource"] : null,
-            "result" => $result,
-            "total_count"=>(isset($this->attribute_holder["total_count"])) ? $this->attribute_holder["total_count"] : null,
-            "sql" => isset($parse["datasource"]["statement"]) ? true : false,
-            "foreign_keys" => $fks,
-            "select_method" => $select_method,
-            "criteria" => $criteria, 
-            "class" => str_replace("Peer","",$data_class),
-            "columns" => $parse["display"]["visible"],
-            "selectors" => (isset($parse["display"]["selectors"])) ? $parse["display"]["selectors"] : null,
-            "proxy" => $parse["proxy"],
-            "limit" => $limit,
-            "rowactions" => isset($parse["rowactions"]) ? $parse["rowactions"] : null,
-            "actions" => isset($parse["actions"]) ? $parse["actions"] : null,
-            "remoteLoad" => ArrayUtil::isTrue($parse, 'remoteLoad'),
-            "remoteSort" => (isset($parse["remoteSort"]) && $parse["remoteSort"] == "true") ? true : false,
-            "remoteFilter" => (isset($parse["remoteFilter"]) && $parse["remoteFilter"] == "true") ? true : false,
-            "exportFrom" => isset($parse["exportFrom"]) ? $parse["exportFrom"] : false,
-            "conditions" => isset($parse["conditions"]) ? $parse["conditions"] : false,
-            "static_real_pagination"=>isset($parse["params"]["static_real_pagination"]) ? $parse["params"]["static_real_pagination"] : false,
-            "reload_topic" => ArrayUtil::get($parse, 'params', 'reload_topic', null),
-        );
-
-        //TODO: parse the datasource from the XML config
-        return $this->renderText('Not implemented yet');
-    }
-
     private static function createDataSource($view) {
         $listView = new afListView($view);
         $selectedColumns = $listView->getSelectedColumns();
 
-        //TODO: support also the other datasources: file and static
+        //TODO: support also the file datasource
         $sourceType = $view->get('datasource@type');
         if($sourceType === 'orm') {
             list($callback, $params) = self::getDataSourceCallback($view);
