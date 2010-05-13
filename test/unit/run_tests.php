7e8eb3c4a41ffb661acb2680f7fee6c325c91894
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-05-13 09:16:34 +0000

    Improved the console output.

--- test/unit/run_tests.php
+++ test/unit/run_tests.php
@@ -2,6 +2,9 @@
 $dir = dirname(__FILE__);
 $tests = glob("$dir/*Test.php");
 foreach($tests as $test) {
+    // Destructing the old lime_test() first.
+    $t = null;
+
     echo "Running $test\n";
     include($test);
 }
