5856fea80a92a22c5428c3b273f38e06b4528337
ivo@immune.dk <ivo@immune.dk@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-05-17 09:01:30 +0000

    #215 allowed to add multiple params at once.

--- lib/UrlUtil.class.php
+++ lib/UrlUtil.class.php
@@ -5,11 +5,31 @@ class UrlUtil {
      * Returns a new url with the given param added.
      */
     public static function addParam($url, $param, $value) {
-        $suffix = sprintf('%s=%s', $param, urlencode($value));
+        $url = self::prepareForParams($url);
+        return $url.sprintf('%s=%s', $param, urlencode($value));
+    }
+
+    /**
+     * Returns a new url with all the given params added.
+     */
+    public static function addParams($url, $params) {
+        $url = self::prepareForParams($url);
+        $first = true;
+        foreach($params as $param => $value) {
+            if(!$first) {
+                $url .= '&';
+            }
+            $url .= sprintf('%s=%s', $param, urlencode($value));
+            $first = false;
+        }
+        return $url;
+    }
+
+    private static function prepareForParams($url) {
         if (StringUtil::isIn('?', $url)) {
-            return $url.'&'.$suffix;
+            return $url.'&';
         } else {
-            return $url.'?'.$suffix;
+            return $url.'?';
         }
     }
 
