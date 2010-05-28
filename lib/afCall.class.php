0cda21179c816a9dfa972de8a0dabfa3916aa9da
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-05-28 09:53:14 +0000

    i#1912 added a link to the PHP bug.

--- lib/afCall.class.php
+++ lib/afCall.class.php
@@ -3,6 +3,7 @@
 class afCall {
     public static function funcArray($callback, $args) {
         // A workaround to prevent "Segmentation fault"
+        // See http://bugs.php.net/bug.php?id=51329
         if(is_array($callback) && isset($callback[0]) && is_string($callback[0])) {
             class_exists($callback[0]);
         }
