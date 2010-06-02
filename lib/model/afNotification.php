b826ae08c8928788b243d29c70ee5c402a184687
prakash <prakash@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-02 07:11:40 +0000

    added new method

--- lib/model/afNotification.php
+++ lib/model/afNotification.php
@@ -11,8 +11,8 @@ class afNotification extends BaseafNotification
 	public function getHtmlMessage(){
 		return $this->getMessage();
 	}
-	public function getHtmlDetail(){
+	public function getHtmlLinkMessage(){
 		$click = "Ext.getBody().mask('Getting detail information..');Ext.Ajax.request({url:'/appFlower/notificationDetails?id=".$this->getId()."',method:'POST',success: function(response){var win = new Ext.Window({width:600,height:400,autoScroll:true,html:response.responseText}); win.show(); win.center();Ext.getBody().unmask();}})";
-		return "<img src='/images/famfamfam/application_view_list.png' style='cursor:pointer' onclick=\"".($click)."\">";
+		return "<a href='javascript:void(0)' onclick=\"".($click)."\">".$this->getHtmlMessage()."</a>";
 	}
 }
