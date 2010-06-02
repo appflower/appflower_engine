7db1d1923f0c2491b4e4b709e120c473a9dc3af1
radutopala <radutopala@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-02 11:28:20 +0000

    #109 - resolved

--- lib/form/fields/ImmExtjsFieldTextarea.class.php
+++ lib/form/fields/ImmExtjsFieldTextarea.class.php
@@ -9,13 +9,18 @@ class ImmExtjsFieldTextarea extends ImmExtjsField
 		if((isset($attributes['rich'])&&$attributes['rich'])||!isset($attributes['rich']))
 		{		
 			$this->attributes['xtype']='htmleditor';
+			
 			$this->attributes['msgTarget']='side';
 		}
 		else {
 			$this->attributes['xtype']='textarea';
+			
+			$this->attributes['autoHeight']=true;
+			$this->attributes['grow']=true;
+			$this->attributes['growMin']=60;
 		}
 		$this->attributes['anchor']='97%';
-		$this->attributes['height']='200';
+		$this->attributes['boxMinHeight']=60;		
 		
 		parent::__construct($fieldsetObject,$attributes);
 	}
