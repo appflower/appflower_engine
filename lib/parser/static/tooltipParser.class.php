7626bbeee758552f5fbe4b995efeb0575aff4c77
gtamas <gtamas@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-03 13:06:01 +0000

    #301 renamed comment and help parsers, schema updated

--- lib/parser/static/tooltipParser.class.php
+++ lib/parser/static/tooltipParser.class.php
@@ -1,12 +1,12 @@
 <?php
 
-class helpParser extends XmlBaseElementParser {
+class tooltipParser extends XmlBaseElementParser {
 	
 	public static function parse($node,$parent,$key = null) {
 		
 		$name = self::$parser->get($parent,"name");
-		self::add("fields/".$name."/comment",self::$parser->get($node));
-			
+		self::add("fields/".$name."/help",self::$parser->get($node));
+		
 		return true;
 		
 	}
