660f7bf8068dc8aeaa59d33439620ce3552d0f0a
prakash <prakash@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-04 04:38:17 +0000

    Notification to trunk

--- web/extjs-3/examples/plugins/Ext.ux.Notification.js
+++ web/extjs-3/examples/plugins/Ext.ux.Notification.js
@@ -11,7 +11,7 @@ Ext.ux.Notification = Ext.extend(Ext.Window, {
 		Ext.apply(this, {
 			iconCls: this.iconCls || 'icon-notification-info',
 			autoHeight: true,
-			closable: true,
+			closeAction:'close',			
 			plain: false,
 			draggable: false,
 			bodyStyle: 'text-align:left;padding:10px;',
@@ -49,7 +49,7 @@ Ext.ux.Notification = Ext.extend(Ext.Window, {
 			this.task.delay(this.hideDelay || 5000);
 		}
 	},
-	animShow : function(){
+	animShow : function(){			
 		this.pos = 0;
 		var h = 0;
 		while(Ext.ux.NotificationMgr.positions.indexOf(this.pos)>-1){
@@ -68,18 +68,69 @@ Ext.ux.Notification = Ext.extend(Ext.Window, {
 	},
 	animHide : function(){
 		Ext.ux.NotificationMgr.positions.remove(this.pos);
+		if(this.el)
 		this.el.ghost("b", {
 			duration: 1
-			, remove: true
-		});
+			, remove: false
+		});		
 	},
 	start: function(url){
 		new Ext.ux.Notification.start(url);
 	}
 });
+Ext.ns("Ext.ux.Notification");
+Ext.ux.Notification.Base = function(){};
+Ext.ux.Notification.Base = Ext.extend(Ext.util.Observable,{
+	showNotification:  function(config) {
+	    var win = new Ext.ux.Notification(Ext.apply({
+	    	animateTarget: this.notificationEl
+			, autoDestroy: false					
+			,notificationType:'ERROR'
+	    }, config));
+	    if(win.notificationType == "ERROR"){
+	    	win.iconCls = 'icon-notification-error';
+	    	win.bodyStyle += 'background-color:#fddcdc;';
+	    }
+	    if(win.notificationType == "WARNING"){
+	    	win.iconCls = 'icon-notification-warning';
+	    	win.bodyStyle += 'background-color:#fefcc7;';
+	    }
+	    if(win.notificationType == "INFO"){
+	    	win.iconCls = 'icon-notification-info';
+	    	win.bodyStyle += 'background-color:#fff;';
+	    }
+	    win.bodyStyle += 'padding-bottom:20px;';	    
+	    win.show();	   
+	    return win;
+	},
+	hideNotification: function(win, delay) {
+	    if (win) {
+	      (function() { 
+	        win.animHide();	        
+	      }).defer(delay || 3000);
+	    }
+	},
+	growl: function(TITLE,MESSAGE,TYPE,DURATION){			
+		var n = this.showNotification({
+			width:300,	
+			notificationType:TYPE,
+			title:TITLE,
+			html:MESSAGE
+		});
+		this.hideNotification(n, DURATION*1000);			
+	}
+})
+Ext.ux.InstantNotification = Ext.extend(Ext.ux.Notification.Base,{
+	constructor: function(config) {	
+		Ext.ux.InstantNotification.superclass.constructor.call(this, config);		
+		this.notificationEl = Ext.get("growl-notification-el")?Ext.get("growl-notification-el"):Ext.DomHelper.append(Ext.getBody(),{tag:'div',id:'growl-notification-el',style:'width:300px;position:absolute;bottom:0;right:0'});
+		this.growl(config.title,config.message,"INFO",10)
+	}
+});
+
 Ext.ux.Notification.start = function(url){	
 	Ext.onReady(function(){	
-		var notificationEl = Ext.DomHelper.append(Ext.getBody(),{tag:'div',style:'width:300px;position:absolute;bottom:0;right:0'});			
+		var notificationEl = Ext.get("growl-notification-el")?Ext.get("growl-notification-el"):Ext.DomHelper.append(Ext.getBody(),{tag:'div',id:'growl-notification-el',style:'width:300px;position:absolute;bottom:0;right:0'});			
 		this.showNotification = function(config) {
 		    var win = new Ext.ux.Notification(Ext.apply({
 		    	animateTarget: notificationEl
