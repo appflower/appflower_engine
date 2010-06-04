ff51328a7d0ba1f6de1674d8b54abcd3f7be844b
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-04 07:00:43 +0000

    #216 allowed to measure the speed from console.

---
+++ test/unit/XmlParserSpeedTest.php
@@ -0,0 +1,20 @@
+<?php
+require_once(dirname(__FILE__).'/../bootstrap/dbunit.php');
+
+Console::$profilingEnabled = true;
+register_shutdown_function(array('Console', 'profile'), 'end');
+
+class AuthenticatedBrowser extends sfBrowser {
+    protected function doCall() {
+        $this->context = $this->getContext(true);
+        $admin = sfGuardUserPeer::retrieveByPk(1);
+        $this->context->getUser()->signIn($admin);
+        ob_start();
+        $this->context->getController()->dispatch();
+        $html = ob_get_clean();
+    }
+}
+
+$browser = new AuthenticatedBrowser();
+$browser->get('/server/listServer');
+
