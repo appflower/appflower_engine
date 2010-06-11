82b24d39bd8be56ea9ce784158c8572c523328ca
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-11 07:23:59 +0000

    #312 tested sfValidatorSchemaCompare usage.

--- test/unit/afValidatorFactoryTest.php
+++ test/unit/afValidatorFactoryTest.php
@@ -1,12 +1,12 @@
 <?php
 include(dirname(__FILE__).'/../bootstrap/dbunit.php');
-$t = new lime_test(5, new lime_output_color());
+$t = new lime_test(9, new lime_output_color());
 
 function assertError($validator, $value, $expectedMsg) {
     global $t;
     try {
         $validator->clean($value);
-        $t->fail();
+        $t->fail('expected: '.$expectedMsg);
     } catch(sfValidatorError $e) {
         $t->is($e->getMessage(), $expectedMsg);
     }
@@ -28,4 +28,14 @@ assertError($validator, '', 'myRequiredError');
 $validator = afValidatorFactory::createValidator('immValidatorHostname',
     array('required'=>'false'));
 $t->is($validator->clean(''), '');
+
+$validator = afValidatorFactory::createValidator('sfValidatorSchemaCompare',
+    array('left_field'=>'password', 'right_field'=>'re_password',
+    'operator'=>'==', 'invalid_error'=>'myInvalidError'));
+$t->is($validator->getOption('left_field'), 'password');
+$t->is($validator->getOption('right_field'), 're_password');
+$t->is($validator->getOption('operator'), '==');
+
+assertError($validator,
+    array('password'=>'hello', 're_password'=>'hello2'), 'myInvalidError');
 
