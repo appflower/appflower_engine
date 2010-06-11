80cc08f9a0d8ab60b987c02e133a88e5b2bec890
radutopala <radutopala@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-11 08:28:33 +0000

    added widget load to grid row actions, use 'load':'page' param if you want to redirect to an entire page

--- web/extjs-3/examples/rowactionsImm/js/Ext.ux.GridRowActions.js
+++ web/extjs-3/examples/rowactionsImm/js/Ext.ux.GridRowActions.js
@@ -363,12 +363,12 @@ Ext.extend(Ext.ux.GridRowActions, Ext.util.Observable, {
 			if(a.script){
 				var urlStart = a.urlIndex ? ('<tpl if="this.isUrl(' + a.urlIndex + ')"><a href="#" onclick="'+a.script+'">') : '';
 			}else if(a.name&&a.name.match("_expand$")){
-				var urlStart = a.urlIndex ? ('<tpl if="this.isUrl(' + a.urlIndex + ')"><a href="{' + a.urlIndex + '}" <tpl if="'+a.confirm+'">onclick="Ext.Msg.confirm(\'Confirmation\',\''+a.message+'\', function(btn){if (btn==\'yes\'){ window.location.href=\'{' + a.urlIndex + '}\'; return false; }else{ return true;}});return false;"</tpl>>') : '';
+				var urlStart = a.urlIndex ? ('<tpl if="this.isUrl(' + a.urlIndex + ')"><a <tpl if="!'+a.confirm+'">class="widgetLoad"</tpl> href="{' + a.urlIndex + '}" <tpl if="'+a.confirm+'">onclick="Ext.Msg.confirm(\'Confirmation\',\''+a.message+'\', function(btn){if (btn==\'yes\'){ afApp.load(\'{' + a.urlIndex + '}\',\'{' + a.load + '}\'); return false; }else{ return true;}});return false;"</tpl>>') : '';
 				if(a.style)	a.style += ";background-image:url(/images/plus.png);background-position:center;background-repeat:no-repeat;";
 				else a.style = "background-image:url(/images/plus.png);background-position:center;background-repeat:no-repeat;";
 				a.iconCls = "imm-expand-row";
 			}else{
-				var urlStart = a.urlIndex ? ('<tpl if="this.isUrl(' + a.urlIndex + ')"><a class="widgetLoad" href="{' + a.urlIndex + '}" <tpl if="'+a.confirm+'">onclick="Ext.Msg.confirm(\'Confirmation\',\''+a.message+'\', function(btn){if (btn==\'yes\'){ window.location.href=\'{' + a.urlIndex + '}\'; return false; }else{ return true;}});return false;"</tpl>>') : '';
+				var urlStart = a.urlIndex ? ('<tpl if="this.isUrl(' + a.urlIndex + ')"><a <tpl if="!'+a.confirm+'">class="widgetLoad"</tpl> href="{' + a.urlIndex + '}" <tpl if="'+a.confirm+'">onclick="Ext.Msg.confirm(\'Confirmation\',\''+a.message+'\', function(btn){if (btn==\'yes\'){ afApp.load(\'{' + a.urlIndex + '}\',\'{' + a.load + '}\'); return false; }else{ return true;}});return false;"</tpl>>') : '';
 			}			
 			
 			/*
