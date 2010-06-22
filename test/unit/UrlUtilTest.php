2d6d0ff51818cbaa7d28272f3a729a982b94b3ea
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-22 14:15:58 +0000

    #374 - fixed the path.

--- test/unit/UrlUtilTest.php
+++ test/unit/UrlUtilTest.php
@@ -1,5 +1,5 @@
 <?php
-include(dirname(__FILE__).'/../../bootstrap/dbunit.php');
+include(dirname(__FILE__).'/../bootstrap/dbunit.php');
 $t = new lime_test(7, new lime_output_color());
 
 $t->is(UrlUtil::addParam('http://hello', 'token', '1234'),
