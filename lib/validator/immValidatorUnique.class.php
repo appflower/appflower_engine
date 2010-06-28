ba5c9ba7ad7ad90ec5a24143bdfe03c1721b0fe8
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-28 16:21:22 +0000

    #374 - allowed to use edit[id] instead of edit[0][id].

--- lib/validator/immValidatorUnique.class.php
+++ lib/validator/immValidatorUnique.class.php
@@ -27,6 +27,7 @@ class immValidatorUnique extends sfValidatorBase
 		$id = sfContext::getInstance()->getRequest()->getParameter("edit[0][id]",false);
 		if(!$id) $id = sfContext::getInstance()->getRequest()->getParameter("edit[1][id]",false);
 		if(!$id) $id = sfContext::getInstance()->getRequest()->getParameter("edit[2][id]",false);
+		if(!$id) $id = sfContext::getInstance()->getRequest()->getParameter("edit[id]",false);
 		
 		$c = new Criteria();
 		$c->add(constant($this->getOption('class')."Peer::".strtoupper($this->getOption('column'))),$value);
