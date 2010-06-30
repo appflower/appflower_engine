0d6b9f923cf74f1bef58b6ad13eb4fbb11fa1fce
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-06-30 08:16:05 +0000

    #374 - used the same client code as for the documentation.

--- test/unit/restClient.php
+++ test/unit/restClient.php
@@ -4,39 +4,22 @@ error_reporting(E_ALL);
 $formUrl = 'https://localhost/networkmonitor_snmp/editSnmpGroup?id=1';
 $apikey = 'RPQgOL2Pwgj06P4mkWHnip2iZMc~admin';
 
-function debug($msg) {
-    file_put_contents('php://stderr', $msg."\n");
-}
-
-function error($reason) {
-    file_put_contents('php://stderr', $reason."\n");
-    exit(1);
-}
-
-function prepareForParams($url) {
-    if (false === strpos($url, '?')) {
-        return $url.'?';
-    } else {
-        return $url.'&';
-    }
-}
-
-$url = prepareForParams($formUrl).'af_format=json&af_apikey='.urlencode($apikey);
-debug("Fetching: $url");
+$url = $formUrl.'&af_format=json&af_apikey='.urlencode($apikey);
 $response = file_get_contents($url);
 if ($response === false) {
-    error('Unable to fetch the URL: '.$url);
+    throw new Exception('Unable to fetch the URL: '.$url);
 }
+
+echo "JSON response: $response\n";
 
 $json = json_decode($response, true);
 if ($json === null) {
-    error('Unable to decode the JSON response: '.$response);
+    throw new Exception('Unable to decode the JSON response: '.$response);
 }
 
-debug('JSON response: '.var_export($json, true));
-
-$submitUrl = prepareForParams($json['af_submitUrl']).'af_apikey='.urlencode($apikey);
+$submitUrl = $json['af_submitUrl'].'?af_apikey='.urlencode($apikey);
 $data = $json;
+$data['edit[name]'] = 'Changed name';
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $submitUrl);
@@ -47,17 +30,21 @@ curl_setopt($ch, CURLOPT_HEADER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
-debug('Sending data to: '.$submitUrl);
 $result = curl_exec($ch);
 if ($result === false) {
-    error('Unable to post data: '.curl_error($ch));
+    throw new Exception('Unable to post data: '.curl_error($ch));
 }
 
 curl_close($ch);
 
+echo "JSON result: $result\n";
+
 $json = json_decode($result, true);
 if ($json === null) {
-    error('Unable to decode the JSON result: '.$result);
+    throw new Exception('Unable to decode the JSON result: '.$result);
 }
 
-debug('JSON result: '.var_export($json, true));
+if (!isset($json['success']) || $json['success'] !== true) {
+    throw new Exception('A form validation error: '.$result);
+}
+
