a87169bb72668a549ad2c1619c46a9b1559754dc
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-07-05 08:29:51 +0000

    i#2047 - backported afRateLimit from AF trunk.

---
+++ lib/filter/afRateLimit.class.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * A set of functions to limit the number of failed submits of a form.
+ * The attacker is identified by his IP address.
+ */
+class afRateLimit {
+    private static $SIN_TTL_SECONDS = 3600;
+    private static $NEEDED_NUM_SINS = 3;
+
+    /**
+     * Returns false if captcha was needed and wasn't valid.
+     */
+    public static function verifyCaptchaIfNeeded($request, $fieldName) {
+        if (!self::isCaptchaNeeded($request)) {
+            return true;
+        }
+
+        $inputValue = $request->getParameter($fieldName);
+        $captcha = new Captcha();
+        if($inputValue && $captcha->verify($inputValue)) {
+            self::clearSins($request);
+            return true;
+        } else {
+            // Disallow to reuse the captcha.
+            $captcha->Set('');
+            return false;
+        }
+    }
+
+    public static function isCaptchaNeeded($request) {
+        if(!self::checkApcSupport()) {
+            return false;
+        }
+
+        return self::getNumSins($request) >= self::$NEEDED_NUM_SINS;
+    }
+
+    public static function rememberSin($request) {
+        if(!self::checkApcSupport()) {
+            return;
+        }
+
+        $numSins = self::getNumSins($request);
+        $key = self::getAttackerId($request);
+        apc_store($key, $numSins + 1, self::$SIN_TTL_SECONDS);
+    }
+
+    private static function clearSins($request) {
+        $key = self::getAttackerId($request);
+        apc_delete($key);
+    }
+
+    private static function getNumSins($request) {
+        $key = self::getAttackerId($request);
+        $numSins = apc_fetch($key);
+        if($numSins === false) {
+            $numSins = 0;
+        }
+        return $numSins;
+    }
+
+    private static function getAttackerId($request) {
+        if(!$request) {
+            throw new Exception('invalid request');
+        }
+        return 'afRateLimit_'.$request->getRemoteAddress();
+    }
+
+    private static function checkApcSupport() {
+        if (function_exists('apc_fetch')) {
+            return true;
+        }
+
+        error_log('Install APC to support attack rate limiting!');
+        return false;
+    }
+}
