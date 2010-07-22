4f97466a30beb2dbdb840a28490cb7923a3b5740
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-07-22 08:20:38 +0000

    i#2050 - allowed to see the wrong line.

--- lib/afErrorHandler.class.php
+++ lib/afErrorHandler.class.php
@@ -5,12 +5,33 @@
  */
 class afErrorHandler {
     private $intro;
+    private $debug;
 
-    public function __construct($intro) {
+    public function __construct($intro, $debug=false) {
         $this->intro = $intro;
+        $this->debug = $debug;
     }
 
-    public function handler($errno, $errstr) {
+    public function handler($errno, $errstr, $errfile, $errline) {
+        if ($this->debug) {
+            $errstr .= "\n in $errfile line $errline:";
+            $errstr .= "\n".self::getLine($errfile, $errline);
+        }
         throw new Exception($this->intro . $errstr);
+    }
+
+    private static function getLine($file, $line) {
+        $fd = fopen($file, 'r');
+        if ($fd === false) {
+            return '';
+        }
+
+        for ($i = 1; $i < $line; $i++) {
+            fgets($fd);
+        }
+
+        $line = fgets($fd);
+        fclose($file);
+        return $line;
     }
 }
