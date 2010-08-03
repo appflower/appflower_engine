7533d9b2c1963f35e8306193a9c2e71ad03aabc9
lukaswoj <lukaswoj@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-08-03 11:33:43 +0000

    #512 - I think that users of seedControl should never see any debug data like for example "message" of PropelException. In this commit I'm detecting errors coming from SQL when runned query has problems related to table relations. I also return more human readable error message instead of message that comes from SQL but only when debug mode is disabled.

--- lib/filter/ExceptionCatchingFilter.class.php
+++ lib/filter/ExceptionCatchingFilter.class.php
@@ -8,21 +8,40 @@ class ExceptionCatchingFilter extends sfFilter
 {
 	public function execute ($filterChain)
 	{
+            
 		$request = $this->context->getRequest();
 		if($request->isXmlHttpRequest() && $this->isFirstCall())
 		{
+                    
 			try {
 				$filterChain->execute();
 			} catch (sfException $e) {
 				throw $e;
+			} catch (PropelException $e) {
+                            $cause = $e->getCause();
+                            $debugMode = sfConfig::get('sf_debug');
+                            if ($cause->getCode() == 23000) {
+                                if (!$debugMode) {
+                                    $message = 'You are probably trying to delete or update a record that have other related records.';
+                                }
+                            }
+                            if (!isset($message)) {
+                                $message = $e->getMessage();
+                            }
+                            $this->injectErrorIntoResponse($message);
 			} catch (Exception $e) {
-				$response = $this->context->getResponse();
-				$params = array('success' => false,
-					'message' => 'Unable to do the action.<br/>Reason: '.$e->getMessage());
-				$response->setContent(json_encode($params));
+                            $this->injectErrorIntoResponse($e->getMessage());
 			}
 		} else {
 			$filterChain->execute();
 		}
 	}
+
+        private function injectErrorIntoResponse($errorMessage)
+        {
+            $response = $this->context->getResponse();
+            $params = array('success' => false,
+                    'message' => 'Unable to do the action.<br/>Reason: '.$errorMessage);
+            $response->setContent(json_encode($params));
+        }
 }
