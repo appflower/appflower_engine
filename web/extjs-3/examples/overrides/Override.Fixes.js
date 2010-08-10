72fc1e7c8de6a893eabfee816379928acf0b9243
prakash <prakash@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-08-10 10:07:01 +0000

    #126 icon and iconCls are both supported now plus iconCls with sprite image

--- web/extjs-3/examples/overrides/Override.Fixes.js
+++ web/extjs-3/examples/overrides/Override.Fixes.js
@@ -67,44 +67,173 @@ Ext.override(Ext.Panel, {
 		}
 	}
 });
-Ext.override(Ext.TabPanel, {
-	onRender : function(ct, position){
-        Ext.TabPanel.superclass.onRender.call(this, ct, position);
+Ext.override(Ext.TabPanel, {	
+    initTab : function(item, index){
+    	
+    	/**
+    	 * Fixes for iconCls, icon with sprite image
+    	 */
+    	var src = Ext.BLANK_IMAGE_URL;
+    	var iconCls = "";
+    	
+    	if(item.icon && item.icon != ''){
+    		src = item.icon;    		
+    	}
+    	if(item.iconCls && item.iconCls != ''){
+    		iconCls = "{iconCls}";
+    	}    	
+    	if(item.icon || item.iconCls){
+	    	var tt = new Ext.Template(
+	    		'<li class="{cls}" id="{id}" style="overflow:hidden">',
+	    	         '<tpl if="closable">',
+	    	            '<a class="x-tab-strip-close"></a>',
+	    	         '</tpl>',
+	    	         '<a class="x-tab-right" href="#" style="padding-left:6px">',
+	    	            '<em class="x-tab-left">',
+	    	                '<span class="x-tab-strip-inner">',
+	    	                    '<img src="'+src+'" class="x-tab-strip-text '+iconCls+'" width="16" height="16" style="padding:0px;float:left;margin-top:2px; margin-right:4px">',
+	    	                    '<span style="'+(!iconCls?"margin-left:20px":"")+'" class="x-tab-strip-text ">{text} {extra}</span>',
+	    	                '</span>',
+	    	            '</em>',
+	    	        '</a>',
+	    	    '</li>'
+			);	    	
+			tt.disableFormats = true;
+			tt.compile();
+			Ext.TabPanel.prototype.itemTpl = tt;
+    	}
+		/***********************************************************/
+        var before = this.strip.dom.childNodes[index],
+            p = this.getTemplateArgs(item),
+            el = before ?
+                 this.itemTpl.insertBefore(before, p) :
+                 this.itemTpl.append(this.strip, p),
+            cls = 'x-tab-strip-over',
+            tabEl = Ext.get(el);
 
-        if(this.plain){
-            var pos = this.tabPosition == 'top' ? 'header' : 'footer';
-            this[pos].addClass('x-tab-panel-'+pos+'-plain');
+        tabEl.hover(function(){
+            if(!item.disabled){
+                tabEl.addClass(cls);
+            }
+        }, function(){
+            tabEl.removeClass(cls);
+        });
+
+        if(item.tabTip){
+            tabEl.child('span.x-tab-strip-text', true).qtip = item.tabTip;
         }
+        item.tabEl = el;
 
-        var st = this[this.stripTarget];
+        // Route *keyboard triggered* click events to the tab strip mouse handler.
+        tabEl.select('a').on('click', function(e){
+            if(!e.getPageX()){
+                this.onStripMouseDown(e);
+            }
+        }, this, {preventDefault: true});
 
-        this.stripWrap = st.createChild({cls:'x-tab-strip-wrap', cn:{
-            tag:'ul', cls:'x-tab-strip x-tab-strip-'+this.tabPosition}});
-
-        var beforeEl = (this.tabPosition=='bottom' ? this.stripWrap : null);
-        st.createChild({cls:'x-tab-strip-spacer'}, beforeEl);
-        this.strip = new Ext.Element(this.stripWrap.dom.firstChild);
-
+        item.on({
+            scope: this,
+            disable: this.onItemDisabled,
+            enable: this.onItemEnabled,
+            titlechange: this.onItemTitleChanged,
+            iconchange: this.onItemIconChanged,
+            beforeshow: this.onBeforeShowItem
+        });
+    }
+});
+/*!
+ * Ext JS Library 3.2.1
+ * Copyright(c) 2006-2010 Ext JS, Inc.
+ * licensing@extjs.com
+ * http://www.extjs.com/license
+ */
+/**
+ * @class Ext.Loader
+ * @singleton
+ * Simple class to help load JavaScript files on demand
+ */
+Ext.Loader = Ext.apply({}, {
+    /**
+     * Loads a given set of .js files. Calls the callback function when all files have been loaded
+     * Set preserveOrder to true to ensure non-parallel loading of files if load order is important
+     * @param {Array} fileList Array of all files to load
+     * @param {Function} callback Callback to call after all files have been loaded
+     * @param {Object} scope The scope to call the callback in
+     * @param {Boolean} preserveOrder True to make files load in serial, one after the other (defaults to false)
+     */
+    load: function(fileList, callback, scope, preserveOrder) {
+        var scope       = scope || this,
+            head        = document.getElementsByTagName("head")[0],
+            fragment    = document.createDocumentFragment(),
+            numFiles    = fileList.length,
+            loadedFiles = 0,
+            me          = this;
         
-        this.edge = this.strip.createChild({tag:'li', cls:'x-tab-edge', cn: [{tag: 'span', cls: 'x-tab-strip-text', cn: '&#160;'}]});
-        this.strip.createChild({cls:'x-clear'});
-
-        this.body.addClass('x-tab-panel-body-'+this.tabPosition);
-
-        
-        //if(!this.itemTpl)
-		{
-            var tt = new Ext.Template(
-                 '<li class="{cls}" id="{id}"><a class="x-tab-strip-close"></a>',
-                 '<a class="x-tab-right" href="#"><em class="x-tab-left">',
-                 '<span class="x-tab-strip-inner"><img src="'+Ext.BLANK_IMAGE_URL+'" class="x-tab-strip-text {iconCls}" width="16" height="16" style="float:left"><span class="x-tab-strip-text">{text}</span></span>',
-                 '</em></a></li>'
+        /**
+         * Loads a particular file from the fileList by index. This is used when preserving order
+         */
+        var loadFileIndex = function(index) {
+            head.appendChild(
+                me.buildScriptTag(fileList[index], onFileLoaded)
             );
-            tt.disableFormats = true;
-            tt.compile();
-            Ext.TabPanel.prototype.itemTpl = tt;
+        };
+        
+        /**
+         * Callback function which is called after each file has been loaded. This calls the callback
+         * passed to load once the final file in the fileList has been loaded
+         */
+        var onFileLoaded = function() {
+            loadedFiles ++;
+            
+            //if this was the last file, call the callback, otherwise load the next file
+            if (numFiles == loadedFiles && typeof callback == 'function') {
+                callback.call(scope);
+            } else {
+                if (preserveOrder === true) {
+                    loadFileIndex(loadedFiles);
+                }
+            }
+        };
+        
+        if (preserveOrder === true) {
+            loadFileIndex.call(this, 0);
+        } else {
+            //load each file (most browsers will do this in parallel)
+            Ext.each(fileList, function(file, index) {
+                fragment.appendChild(
+                    this.buildScriptTag(file, onFileLoaded)
+                );  
+            }, this);
+            
+            head.appendChild(fragment);
         }
-
-        this.items.each(this.initTab, this);
+    },
+    
+    /**
+     * @private
+     * Creates and returns a script tag, but does not place it into the document. If a callback function
+     * is passed, this is called when the script has been loaded
+     * @param {String} filename The name of the file to create a script tag for
+     * @param {Function} callback Optional callback, which is called when the script has been loaded
+     * @return {Element} The new script ta
+     */
+    buildScriptTag: function(filename, callback) {
+        var script  = document.createElement('script');
+        script.type = "text/javascript";
+        script.src  = filename;
+        
+        //IE has a different way of handling <script> loads, so we need to check for it here
+        if (script.readyState) {
+            script.onreadystatechange = function() {
+                if (script.readyState == "loaded" || script.readyState == "complete") {
+                    script.onreadystatechange = null;
+                    callback();
+                }
+            };
+        } else {
+            script.onload = callback;
+        }    
+        
+        return script;
     }
 });
