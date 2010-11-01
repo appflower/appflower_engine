fb01c2d1a72956a9f00a92a4c203dbb7ba768428
prakash <prakash@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-11-01 09:09:49 +0000

    Fixed the captcha reload/input field for captcha in IE6

--- web/extjs-3/examples/form/Ext.ux.plugins.Captcha.js
+++ web/extjs-3/examples/form/Ext.ux.plugins.Captcha.js
@@ -15,7 +15,7 @@ Ext.ux.Captcha = Ext.extend(Ext.form.TextField,  {
         src = this.src;
         captchaDiv = Ext.DomHelper.insertFirst(ct, {tag: 'div', style:'width:'+this.width+'px; margin:0px; margin-bottom:2px;padding:0px'});
         captchaImgDiv = Ext.DomHelper.insertFirst(captchaDiv, {tag: 'div', style:'margin:0px;float:left; padding:0px'});
-        reloadImgDiv = Ext.DomHelper.insertFirst(captchaDiv, {tag: 'div', html:'<div style="color:#888; padding-bottom:2px;">Can\'t see image? <a href="javascript:void(0)" onclick="var id = randId;var obj = document.getElementById(id);var src = obj.src;var pos = src.indexOf(\'?\');if (pos >= 0) {src = src.substr(0, pos);}var date = new Date();obj.src = src + \'?v=\' + date.getTime();return false;" style="color:#0000ff">Reload</a></div>',  style:'margin:0px;float:right; padding:0px'});
+        reloadImgDiv = Ext.DomHelper.insertFirst(captchaDiv, {tag: 'div', html:'<div style="color:#888; padding-bottom:2px;">Can\'t see image? <a href="javascript:void(0)" onclick="var id = randId;var obj = document.getElementById(id);var src = obj.src;var pos = src.indexOf(\'?\');if (pos >= 0) {src = src.substr(0, pos);}var date = new Date();obj.src = src + \'?v=\' + date.getTime();this.blur();return false;" style="color:#0000ff">Reload</a></div>',  style:'margin:0px;float:right; padding:0px'});
         captchaImg = Ext.DomHelper.append(captchaImgDiv, {tag: 'img', id:randId, src:this.src, width:this.width,height:30,style:this.imgStyle});  
         if(!this.input){
         	this.el.applyStyles({display:'none'});
