12a6ae83bb37a72e689c7d38a7c428c724637b87
ivo <ivo@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2010-12-21 13:14:41 +0000

    i#2292 - made requiredValidator compatible with symfony 1.4.

--- lib/validator/requiredValidator.class.php
+++ lib/validator/requiredValidator.class.php
@@ -1,30 +1,5 @@
 <?php
-class requiredValidator extends sfValidator
+class requiredValidator extends immValidatorRequired
 {
-	public function execute(&$value, &$error)
-	{		 
-		if (trim($value) == "")
-		{
-			$error = $this->getParameter('required_error');
-			return false;
-		}
-
-		return true;
-	}
-
-	public function initialize ($context, $parameters = null)
-	{
-		
-		// Initialize parent
-		parent::initialize($context);
-
-		// Set default parameters value
-		$this->setParameter('required_error', 'This field is required!');
-
-		// Set parameters
-		$this->getParameterHolder()->add($parameters);
-
-		return true;
-	}
 }
 ?>
