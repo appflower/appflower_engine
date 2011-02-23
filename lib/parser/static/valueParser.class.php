d0b8b7f42fe9c51c98f7de6c031c3cc086180537
lukaswoj <lukaswoj@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2011-02-23 12:30:51 +0000

    valueParser will not try to guess value getter method when value type is set to orm and no other attributes are specified Now field can be defined like this: <i:field name="name" label="Name" type="input">     <i:value type="orm"/> </i:field>

--- lib/parser/static/valueParser.class.php
+++ lib/parser/static/valueParser.class.php
@@ -46,29 +46,43 @@ class valueParser extends XmlBaseElementParser {
 			self::add("fields/".$name."/value/items",$it);
 		
 		} else if($node) {
-			self::add("fields/".$name."/value/type","2");
-			self::add("fields/".$name."/value/class",self::$parser->get(self::$parser->getnode("class",$node)));
-			$method = self::$parser->getnode("method",$node);
-			self::add("fields/".$name."/value/method/name",self::$parser->get($method,"name"));
-			
-			$nodes = self::$parser->fetch("./i:param",$method);
-			$it = new nodeListIterator($nodes);
-				
-			if($it->getListLength() > 0) {
-				self::parseValues($it);
-				$it->rewind();
-				//$it->setMode(VALUES);	
-			}
-			
-			$i = 0;
-			foreach($it as $v) {
-				$val = self::$parser->get($v);
-				if(!property_exists($v,"non_convertable") && substr($val,0,1) == "[") {
-					$val = explode(",",preg_replace("/[\[\]]+/","",$val));
-				} 
-				self::add("fields/".$name."/value/method/params/".$i,$val);	
-			$i++;
-			}
+            $class = self::$parser->get(self::$parser->getnode("class",$node));
+            if ($class) {
+                self::add("fields/".$name."/value/type","2");
+                self::add("fields/".$name."/value/class",$class);
+                $method = self::$parser->getnode("method",$node);
+                if ($method) {
+                    self::add("fields/".$name."/value/method/name",self::$parser->get($method,"name"));
+
+                    $nodes = self::$parser->fetch("./i:param",$method);
+                    $it = new nodeListIterator($nodes);
+
+                    if($it->getListLength() > 0) {
+                        self::parseValues($it);
+                        $it->rewind();
+                        //$it->setMode(VALUES);
+                    }
+
+                    $i = 0;
+                    foreach($it as $v) {
+                        $val = self::$parser->get($v);
+                        if(!property_exists($v,"non_convertable") && substr($val,0,1) == "[") {
+                            $val = explode(",",preg_replace("/[\[\]]+/","",$val));
+                        }
+                        self::add("fields/".$name."/value/method/params/".$i,$val);
+                    $i++;
+                    }
+                }
+            } else if ($type == 'orm') {
+                // falling back to class defined in datasource node with guessed getter method name
+                self::add("fields/".$name."/value/type","1");
+                self::add("fields/".$name."/value/class",$process["parses"][$iteration]["datasource"]["class"]);
+                $nameParts = explode('[', $name);
+                $nameParts = array_reverse($nameParts);
+                $originalName = str_replace(']', '', $nameParts[0]);
+                $originalNameGetterMethod = 'get' . sfInflector::camelize($originalName);
+                self::add("fields/".$name."/value/method",$originalNameGetterMethod);
+            }
 			
 		}
 		
