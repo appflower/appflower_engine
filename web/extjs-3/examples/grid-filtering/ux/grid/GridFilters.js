6727cf5907a10772cc1e32487310c547778586b2
prakash <prakash@ddad7ec1-79ad-4a77-b24b-7ba2f51c7ac0>
2011-03-03 09:46:02 +0000

    #2523 UI The native javascript method Date.parse is dependent on the browser and os. It did not worked for IE. Overwritten the method to support cross browser

--- web/extjs-3/examples/grid-filtering/ux/grid/GridFilters.js
+++ web/extjs-3/examples/grid-filtering/ux/grid/GridFilters.js
@@ -1,4 +1,27 @@
 /**
+ * Browser compatible Date.parse
+ */
+(function () {
+    var origParse = Date.parse;
+    Date.parse = function (date) {
+        var timestamp = origParse(date), minutesOffset = 0, struct;
+        if (isNaN(timestamp) && (struct = /^(\d{4}|[+\-]\d{6})-(\d{2})-(\d{2})(?:[T ](\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3,}))?)?(?:(Z)|([+\-])(\d{2})(?::?(\d{2}))?))?/.exec(date))) {
+            if (struct[8] !== 'Z') {
+                minutesOffset = +struct[10] * 60 + (+struct[11]);
+                
+                if (struct[9] === '+') {
+                    minutesOffset = 0 - minutesOffset;
+                }
+            }
+            
+            timestamp = Date.UTC(+struct[1], +struct[2] - 1, +struct[3], +struct[4], +struct[5] + minutesOffset, +struct[6], +struct[7].substr(0, 3));
+        }
+        
+        return timestamp;
+    };
+}());
+
+/**
  * Fix for Ext 3
  */
 Ext.grid.GridView.prototype.handleHdMenuClick = function(item){
