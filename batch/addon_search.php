a70b4e15be535fcf188d6bf1167a482774ce7bfa
Radu Topala <radu@appflower.com>
2011-05-04 18:40:43 +0400

    batch script for creating cactus.xml config file containing all js/css files inside AppFlower Engine

---
+++ batch/addon_search.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * batch for creating cactus.xml containing all js/css files used inside AppFlower Engine
+ * 
+ * @author Radu Topala <radu@appflower.com>
+ */
+
+require_once(dirname(__FILE__).'/../../../config/ProjectConfiguration.class.php');
+
+if(file_exists(dirname(__FILE__).'/../../../plugins/appFlowerStudioPlugin/lib/config/afFilterConfigHandler.class.php'))
+{
+	require_once(dirname(__FILE__).'/../../../plugins/appFlowerStudioPlugin/lib/config/afFilterConfigHandler.class.php');
+}
+
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+sfContext::createInstance($configuration);
+
+$phpFiles = sfFinder::type('file')->name('*.php')->in(sfConfig::get('sf_root_dir').'/plugins/appFlowerPlugin');
+$arrays = array('js'=>array(),'css'=>array());
+
+foreach ($phpFiles as $phpFile)
+{
+	$content = null;
+	$content = file_get_contents($phpFile);
+
+	preg_match_all('/setAddons\((.*)\);/',$content,$matches);
+
+	if(count($matches)>0)
+	{
+		foreach ($matches[1] as $array)
+		{
+			if(substr_count($array,'".$plugin."')>0||substr_count($array,"\$script")>0||substr_count($array,"\$addons")>0)
+			{
+				continue;
+			}
+
+			$array = str_replace('$this->afExtjs->getPluginsDir().\'','\'/appFlowerPlugin/extjs-3/plugins/',$array);
+			$array = str_replace('$grid->afExtjs->getPluginsDir().\'','\'/appFlowerPlugin/extjs-3/plugins/',$array);
+
+			//echo $phpFile.' '.$array."\n";
+			$array = eval("return (".$array.");");
+			foreach ($array as $type=>$typeArray)
+			{
+				$arrays[$type]=array_merge($arrays[$type],$typeArray);
+			}
+		}
+	}
+}
+
+foreach ($arrays as $arrayType=>$array)
+{
+	$arrays[$arrayType]=array_unique($array);
+}
+
+$cactusXml[]="<cactus>\r\n\t<needles>";
+foreach ($arrays as $arrayType=>$array)
+{
+	$cactusXml[]="\r\n\r\n\t\t<!-- AppFlower ".$arrayType." files -->\r\n\t\t<needle>\r\n\t\t\t<output type=\"".$arrayType."\">appFlower.".$arrayType."</output>\r\n\t\t\t<files>";
+	foreach ($array as $file)
+	{
+		$cactusXml[]="\r\n\t\t\t\t<file>".sfConfig::get('sf_root_dir').'/web'.$file."</file>";
+	}
+	$cactusXml[]="\r\n\t\t\t</files>\r\n\t\t</needle>";
+}
+$cactusXml[]="\r\n\t<needles>\r\n<cactus>";
+
+file_put_contents(sfConfig::get('sf_root_dir').'/plugins/appFlowerPlugin/config/cactus.xml',implode(null,$cactusXml));
