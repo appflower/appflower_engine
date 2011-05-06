5a562c6a718b5803a5e6b7c84c2cb035b2e97721
Radu Topala <radu@appflower.com>
2011-05-06 14:30:56 +0400

    modified addon_search batch

--- batch/addon_search.php
+++ batch/addon_search.php
@@ -87,6 +87,6 @@ foreach ($arrays as $arrayType=>$array)
 	}
 	$cactusXml[]="\r\n\t\t\t\t</files>\r\n\t\t\t</needle>\r\n\t\t</needles>\r\n\t</".$arrayType.">";
 }
-$cactusXml[]="\r\n<cactus>";
+$cactusXml[]="\r\n</cactus>";
 
 file_put_contents(sfConfig::get('sf_root_dir').'/plugins/appFlowerPlugin/config/cactus.xml',implode(null,$cactusXml));
