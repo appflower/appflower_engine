3d24e976e30f302619b417616977f4f2bb0c5731
Sergey Startsev <startsev.sergey@gmail.com>
2011-09-07 00:17:17 +0300

    replace Ext.get("body") to Ext.getBody() used in Ext 3.4

--- web/js/custom/save_assets.js
+++ web/js/custom/save_assets.js
@@ -1,6 +1,6 @@
 var grid = center_panel.items.items[0].items.items[1];
 var selected = grid.getSelectionModel().getSelectionsJSON();
-var mask = new Ext.LoadMask(Ext.get("body"), {msg: "Please Wait...", removeMask: true});
+var mask = new Ext.LoadMask(Ext.getBody(), {msg: "Please Wait...", removeMask: true});
 mask.show();
 Ext.Ajax.request({
 	url:'/wizard/saveJson?step=5',
