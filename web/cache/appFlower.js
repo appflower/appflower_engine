dbdb0bf61ba57d8fb698623b616c1f450d0eda72
Radu Topala <radu@appflower.com>
2011-09-20 10:01:19 +0300

    regen cache

--- web/cache/appFlower.js
+++ web/cache/appFlower.js
@@ -22819,6 +22819,8 @@ Ext.ux.StartMenu = Ext.extend(Ext.menu.Menu, {
 
         this.fireEvent("beforeshow", this);
         this.showAt(this.el.getAlignToXY(el, pos || this.defaultAlign), parentMenu, false);
+        this.getEl().setTop(this.getEl().getTop()-5);
+        this.getEl().setLeft(0);
         var tPanelWidth = 100;
         var box = this.menuBWrap.getBox();
         this.menuPanel.setWidth(box.width-tPanelWidth);
