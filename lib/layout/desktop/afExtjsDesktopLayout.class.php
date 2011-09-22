05a708a6c2d91fb90e88e8dbd7391bf3dfba7c90
Radu Topala <radu@appflower.com>
2011-09-22 16:55:12 +0300

    fix for non existent afExtjsDesktopStartMenu helper

--- lib/layout/desktop/afExtjsDesktopLayout.class.php
+++ lib/layout/desktop/afExtjsDesktopLayout.class.php
@@ -8,7 +8,10 @@ class afExtjsDesktopLayout extends afExtjsLayout
 	{
 		$this->afExtjs->setAddons(array ('css' => array($this->afExtjs->getPluginsDir().'desktop/css/desktop.css'), 'js' => array($this->afExtjs->getPluginsDir().'desktop/js/StartMenu.js',$this->afExtjs->getPluginsDir().'desktop/js/TaskBar.js',$this->afExtjs->getPluginsDir().'desktop/js/Desktop.js')));
 		
-		sfProjectConfiguration::getActive()->loadHelpers(array('afExtjsDesktopStartMenu'));
+		if (is_readable(sfConfig::get('sf_app_lib_dir').'/helper/afExtjsDesktopLinksHelper.php'))
+        {
+		  sfProjectConfiguration::getActive()->loadHelpers(array('afExtjsDesktopStartMenu'));
+        }
 		
 		$this->addInitMethodSource("this.startConfig = startMenuConfig;this.desktop = new Ext.Desktop(this);");
 	}
